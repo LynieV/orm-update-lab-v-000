79282bfee74511ba43c46d8d3f67353f241d7cf3
Valerie Lynie McClimon <vmcclimon33@yahoo.com>
2018-08-15 01:39:52 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,13 +4,13 @@ class Student
 
   attr_accessor :name, :grade
     attr_reader :id
-  
+
     def initialize(id=nil, name, grade)
       @name = name
       @grade = grade
       @id = id
     end
-  
+
     def self.create_table
       sql =  <<-SQL
         CREATE TABLE IF NOT EXISTS students (
@@ -21,14 +21,14 @@ class Student
           SQL
       DB[:conn].execute(sql)
     end
-  
+
     def self.drop_table
       sql = <<-SQL
       DROP TABLE students
       SQL
       DB[:conn].execute(sql)
     end
-  
+
     def save
     if self.id
       self.update
@@ -41,27 +41,27 @@ class Student
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
       end
     end
-  
+
     def self.create(name, grade)
       student = Student.new(name, grade)
       student.save
       student
     end
-  
+
     # Remember, you can access your database connection anywhere in this class
     #  with DB[:conn]
     def update
       sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
       DB[:conn].execute(sql, self.name, self.grade, self.id)
     end
-  
+
     def self.new_from_db(row)
         id = row[0]
         name = row[1]
         grade = row[2]
         self.new(id, name, grade)  # self.new is the same as running Song.new
     end
-  
+
     def self.find_by_name(name)
    sql = <<-SQL
      SELECT *
@@ -69,7 +69,7 @@ class Student
      WHERE name = ?
      --LIMIT 1
    SQL
-  
+
    DB[:conn].execute(sql, name).map do |row|
      self.new_from_db(row)
     end.first
